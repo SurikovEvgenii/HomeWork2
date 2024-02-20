b255914f1809f78f5edb204aa29de05e07934951
Evgenii Surikov <surikov96@mail.ru>
2024-02-20 19:50:56 +0700

    Method info() print canonicalName of class toUpperCase().

--- src/main/java/com/surikov/homework1/Service.java
+++ src/main/java/com/surikov/homework1/Service.java
@@ -8,6 +8,6 @@ public class Service {
     }
 
     public void info(){
-        System.out.println(this.getClass().getCanonicalName());
+        System.out.println(this.getClass().getCanonicalName().toUpperCase());
     }
 }
